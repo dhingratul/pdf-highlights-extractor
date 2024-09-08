af1ec77b0fb9d79d727a38c1ada4f86b2f4d668d
dhingratul <dhingra.atul92@gmail.com>
2024-09-07 23:04:32 -0700

    Fix test_data path

--- pdfhextractor/tests/test_pdfhextractor_core.py
+++ pdfhextractor/tests/test_pdfhextractor_core.py
@@ -5,13 +5,13 @@ from pdfhextractor.core import PdfHighlightExtractor
 class TestPdfHighlightExtractor(unittest.TestCase):
     def test_read_one_file(self):
         hex = PdfHighlightExtractor()
-        highlights = hex.read_one_file("pdfhextract/tests/test_data/pose/OpenPose.pdf")
+        highlights = hex.read_one_file("pdfhextractor/tests/test_data/pose/OpenPose.pdf")
         self.assertEqual(len(highlights), 2)
         self.assertEqual(highlights[0], "Part Afﬁnity Fields (PAFs),")
 
     def test_read_directory(self):
         hex = PdfHighlightExtractor()
-        highlights = hex.read_directory("pdfhextract/tests/test_data/pose")
+        highlights = hex.read_directory("pdfhextractor/tests/test_data/pose")
         self.assertEqual(len(highlights), 2)
         self.assertEqual(highlights[0], "Part Afﬁnity Fields (PAFs),")
 
